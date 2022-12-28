9500ac7ce6f5f9dc5d087ff6614773da361312a5
rohith0806 <rohith4204015@gmail.com>
2022-12-28 20:04:17 +0530

    common js to ECMA 6 done 2

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const passport = require('passport');
-const connectDB = require('./config/database');
-const authRoutes = require('./routes/auth');
-const userRoutes = require('./routes/users');
-const examRoutes = require('./routes/exams');
-const mockTestRoutes = require('./routes/mock-tests');
-const paymentRoutes = require('./routes/payments');
-const errorHandler = require('./middlewares/error-handler');
+
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import passport from 'passport';
+import connectDB from './config/database';
+import authRoutes from './routes/auth';
+import teacherRoutes from './routes/teacherRoutes';
+import examRoutes from './routes/examRoutes';
+import paymentRoutes from './routes/paymentRoutes';
+
 
 const app = express();
 
@@ -27,24 +27,12 @@ app.use(passport.initialize());
 
 // Use the auth routes
 app.use('/api/auth', authRoutes);
-
-// Use the user routes
-app.use('/api/users', userRoutes);
-
-// Use the exam routes
+app.use('/api/teacher', teacherRoutes);
 app.use('/api/exams', examRoutes);
-
-// Use the mock test routes
-app.use('/api/mock-tests', mockTestRoutes);
-
-// Use the payment routes
 app.use('/api/payments', paymentRoutes);
-/*
-// Use the error handler middleware
-app.use(errorHandler);
-*/
+
 // Start the server
-const port = process.env.PORT || 5000;
+//const port = process.env.PORT || 5000;
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+console.log(`Server is running on port ${5000}`);
 });
