52747f28d448c5ed32f8d4147f660f02fc23a2a2
rohith0806 <rohith4204015@gmail.com>
2022-12-28 07:49:32 +0530

    commit

--- controllers/mock-tests/mocktestSubmitController.js
+++ controllers/mock-tests/mocktestSubmitController.js
@@ -9,7 +9,7 @@ exports.mocktestSubmit =  async (req, res) => {
     // Find the exam by ID
     const exam = await Exam.findById(req.params.examId);
 
-    // If the exam was not found, return a 404 status code with a message
+    // If tse exam was not found, return a 404 status code with a message
     if (!exam) return res.status(404).send('Exam not found');
 
     // Find the subject by ID
